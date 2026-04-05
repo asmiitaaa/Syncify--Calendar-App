613a3c9419b31ee7b366759796d927ba56b934df
asmiitaaa <asmita20six@gmail.com>
2026-04-06 00:29:45 +0530

    fix email port type

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
+  port: parseInt(process.env.EMAIL_PORT),
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
